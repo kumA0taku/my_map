7d3be80b50c5c0b243e9acd5cf0f07cc9c9c1b20
kumA0taku <varinpimda@gmail.com>
2024-01-09 10:54:48 +0700

    cleansing code

--- src/Components/MapComponents/OpenLayerComponent.js
+++ src/Components/MapComponents/OpenLayerComponent.js
@@ -1,41 +1,98 @@
 import React, { useEffect, useState, useRef } from "react";
 import "ol/ol.css";
-import "ol-popup/src/ol-popup";
+import "ol-popup/src/ol-popup.css";
 import Map from "ol/Map";
 import View from "ol/View";
+import { transform } from "ol/proj";
 import TileLayer from "ol/layer/Tile";
 import OSM from "ol/source/OSM";
-// import {toStringHDMS} from 'ol/coordinate';
+import LayerGroup from "ol/layer/Group";
+import LayerImage from "ol/layer/Image";
+import SourceImageArcGISRest from "ol/source/ImageArcGISRest";
+import TileWMS from 'ol/source/TileWMS';
+import SourceStamen from "ol/source/Stamen";
+import SourceXYZ from "ol/source/XYZ";
+import LayerSwitcher from "ol-layerswitcher";
 
 // Import styling
 import "../../App.css";
+// import { layerGroup } from "leaflet";
 
 const OpenLayerComponent = () => {
   const mapContainerRef = useRef(null);
   const map = useRef(null);
 
-  const [lng] = useState(-97.7431);
-  const [lat] = useState(30.2672);
-  const [zoom] = useState(2);
+  const [lng] = useState(100.6211);
+  const [lat] = useState(15.1346);
+  const [zoom] = useState(5.5284);
 
   useEffect(() => {
-    
     map.current = new Map({
-        target: mapContainerRef.current,
-        layers: [
-          new TileLayer({
-            source: new OSM(),
-          }),
-        ],
-        view: new View({
-          center: [lng, lat],
-          zoom: zoom,
+      target: mapContainerRef.current,
+      layers: [
+        new LayerGroup({
+          title: "Base maps",
+          layers: [
+            new LayerGroup({
+              title: "Water color with labels",
+              type: "base",
+              combine: true,
+              visible: false,
+              layers: [
+                new TileLayer({
+                  source: new SourceStamen({
+                    layer: "watercolor",
+                  }),
+                }),
+                new TileLayer({
+                  source: new SourceStamen({
+                    layer: "terrain-labels",
+                  }),
+                }),
+              ],
+            }),
+            new TileLayer({
+              title: "water color",
+              type: "base",
+              visible: false,
+              source: new SourceStamen({
+                layer: "watercolor",
+              }),
+            }),
+            new TileLayer({
+              title: "OSM",
+              type: "base",
+              visible: true,
+              source: new OSM({}),
+            }),
+          ],
         }),
-      });
-  
-      return () => {
-        map.current.setTarget(null);
-      };
+        new LayerGroup({
+            title: 'Overlays',
+            layers:[
+              new TileLayer({
+                title: 'Countries',
+                source: new SourceXYZ({
+                  ratio: 1,
+                  params: {'LAYERS': 'show:0'},
+                  url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                }),
+              }),
+            ],
+        }),
+      ],
+      view: new View({
+        center: transform([lng, lat], "EPSG:4326", "EPSG:3857"),
+        zoom: zoom,
+      }),
+    });
+
+    const layerSwitcher = new LayerSwitcher();
+    map.current.addControl(layerSwitcher);
+
+    return () => {
+      map.current.setTarget(null);
+    };
   }, [lat, lng, zoom]);
 
   return <div className="map-container" ref={mapContainerRef} />;
--- src/Components/TapComponents/Tabs.js
+++ src/Components/TapComponents/Tabs.js
@@ -2,39 +2,21 @@ import React, { useState } from "react";
 import TabNavItem from "./TabNavItem";
 import TabContent from "./TabContent";
 // Import our Map Components
-import LeafletComponent from "../MapComponents/LeafletComponent";
 import OpenLayerComponent from "../MapComponents/OpenLayerComponent";
-// import OpenLayersComponent from "../MapComponents/OpenLayersComponent";
 
 
 const Tabs = () => {
-  const [activeTab, setActiveTab] = useState("mapbox");
+  const [activeTab, setActiveTab] = useState("openlayers");
 
   return (
     <div className="Tabs">
       <ul className="nav">
-        <TabNavItem title="Mapbox" id="mapbox" activeTab={activeTab} setActiveTab={setActiveTab} />
-        <TabNavItem title="Leaflet" id="leaflet" activeTab={activeTab} setActiveTab={setActiveTab} />
-        <TabNavItem title="OpenLayers" id="openlayers" activeTab={activeTab} setActiveTab={setActiveTab} />
-        <TabNavItem title="ArcGIS" id="arcgis" activeTab={activeTab} setActiveTab={setActiveTab} />
-        <TabNavItem title="Carto" id="carto" activeTab={activeTab} setActiveTab={setActiveTab} />
+        <TabNavItem title="Web map GIS" id="openlayers" activeTab={activeTab} setActiveTab={setActiveTab} />
       </ul>
 
       <div className="outlet">
-        <TabContent id="mapbox" activeTab={activeTab}>
-          <p>Mapbox works!</p>
-        </TabContent>
-        <TabContent id="leaflet" activeTab={activeTab}>
-          <LeafletComponent></LeafletComponent>
-        </TabContent>
         <TabContent id="openlayers" activeTab={activeTab}>
             <OpenLayerComponent></OpenLayerComponent>
-        </TabContent>
-        <TabContent id="arcgis" activeTab={activeTab}>
-          <p>ArcGIS works!</p>
-        </TabContent>
-        <TabContent id="carto" activeTab={activeTab}>
-          <p>Carto works!</p>
         </TabContent>
       </div>
     </div>
